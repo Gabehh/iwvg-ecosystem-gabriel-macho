ae6675cf20c3695c18547bc2288dec93fee538a4
Gabriel Enrique Macho <gabe3195@gmail.com>
2019-09-29 03:34:10 +0200

    Add UserTest #7

---
+++ src/test/java/es/upm/miw/iwvg/ecosystem/practica/UserTest.java
@@ -0,0 +1,56 @@
+package es.upm.miw.iwvg.ecosystem.practica;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class UserTest {
+
+    private User user;
+
+    @BeforeEach
+    void before() {
+        user = new User(1,"Gabriel Macho", "La Familia Macho");
+    }
+    @Test
+    void testNumber(){
+        assertEquals(1, user.getNumber());
+    }
+
+    @Test
+    void testNumberNumber(){
+        user = new User(2,"Gabriel Macho", "La Familia Macho");
+        assertEquals(2, user.getNumber());
+    }
+
+    @Test
+    void testFullName(){
+        assertEquals("Gabriel macho La familia macho", user.fullName());
+    }
+
+    @Test
+    void testFullNameName(){
+        user = new User(2,"Enrique Macho", "La Familia Macho");
+        assertEquals("Enrique macho La familia macho", user.fullName());
+    }
+
+    @Test
+    void testInitials(){
+        assertEquals("G.", user.initials());
+    }
+    @Test
+    void testName(){
+        assertEquals("Gabriel macho", user.getName());
+    }
+
+    @Test
+    void testFamilyName(){
+        assertEquals("La familia macho", user.getFamilyName());
+    }
+
+    @Test
+    void testFamilyNameName(){
+        user = new User(5,"Gabriel Macho", "Nueva Familia Macho");
+        assertEquals("Nueva familia macho", user.getFamilyName());
+    }
+
+}
