5334a129dc56a1da7f96257a188052d881db2cc4
Gabriel Enrique Macho <gabe3195@gmail.com>
2019-09-29 03:51:35 +0200

    Add Fraction #9

---
+++ src/main/java/es/upm/miw/iwvg/ecosystem/practica/Fraction.java
@@ -0,0 +1,30 @@
+package es.upm.miw.iwvg.ecosystem.practica;
+
+public class Fraction {
+
+    private int numerator;
+
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public Fraction() {
+        this(1, 1);
+    }
+
+    public double decimal() {
+        return (double) numerator / denominator;
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+}
